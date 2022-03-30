1ce10729e236dac799e60f9fb06625fbe823f1b8
EscapeFromJava <reske.artur@gmail.com>
2022-03-30 23:31:53 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Сколько A-деревьев в день срубает Дмитрий?");
+        int A = scan.nextInt();
+        System.out.println("Какой К-ый день отдыхает Дмитрий?");
+        int K = scan.nextInt();
+        System.out.println("Сколько B-деревьев в день срубает Федор?");
+        int B = scan.nextInt();
+        System.out.println("Какой M-ый день отдыхает Федор?");
+        int M = scan.nextInt();
+        System.out.println("Сколько растет X-деревьев в лесу?");
+        int X = scan.nextInt();
+        int value = 0;
+        int weekendNikolay = K;
+        int weekendFedor = M;
+        for (int i = 1; i <= X; i++) {
+            if (i != weekendNikolay)
+                value += A;
+            else
+                weekendNikolay += K;
+            if (i != weekendFedor)
+                value += B;
+            else
+                weekendFedor += M;
+            if (value >= X) {
+                System.out.println("Деревья будут вырублены за " + i + " дней");
+                break;
+            }
+        }
+    }
+}
